11018051009aa5ddb21d2877c5245c262979278f
Farah Hamdo <faroha.tatsu@gmail.com>
2021-04-16 11:53:26 +0200

    Grapple Hook implemented the base ( press G to shoot the cable and right click to launch the player ) There is a problem with the math when launching the player. needs to be adjusted in the "Launch character " inside the blueprint

--- Source/Chameleon/BaseGameMode.cpp
+++ Source/Chameleon/BaseGameMode.cpp
@@ -1,5 +1,13 @@
+<<<<<<< Updated upstream
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "BaseGameMode.h"
 
+=======
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BaseGameMode.h"
+
+>>>>>>> Stashed changes
--- Source/Chameleon/BaseGameMode.h
+++ Source/Chameleon/BaseGameMode.h
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
@@ -15,3 +16,22 @@ class CHAMELEON_API ABaseGameMode : public AChameleonGameMode
 	GENERATED_BODY()
 	
 };
+=======
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ChameleonGameMode.h"
+#include "BaseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAMELEON_API ABaseGameMode : public AChameleonGameMode
+{
+	GENERATED_BODY()
+	
+};
+>>>>>>> Stashed changes

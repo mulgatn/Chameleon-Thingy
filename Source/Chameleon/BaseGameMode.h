f8e3f32b059496b40fe71dea2bedf550f4274f7f
Christian Lundgren <christian.carl.lundgren@gmail.com>
2021-04-16 11:31:08 +0200

    Things

---
+++ Source/Chameleon/BaseGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ChameleonGameMode.h"
+#include "BaseGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHAMELEON_API ABaseGameMode : public AChameleonGameMode
+{
+	GENERATED_BODY()
+	
+};

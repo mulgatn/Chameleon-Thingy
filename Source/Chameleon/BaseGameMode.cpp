4c5dc1c0da4ea9439ed8fcc6c8cffc3501712e4b
Farah Hamdo <faroha.tatsu@gmail.com>
2021-04-21 19:43:11 +0200

    Fixed stash error

--- Source/Chameleon/BaseGameMode.cpp
+++ Source/Chameleon/BaseGameMode.cpp
@@ -1,13 +1,4 @@
-<<<<<<< Updated upstream
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "BaseGameMode.h"
-
-=======
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "BaseGameMode.h"
-
->>>>>>> Stashed changes
--- Source/Chameleon/BaseGameMode.h
+++ Source/Chameleon/BaseGameMode.h
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
@@ -16,22 +15,3 @@ class CHAMELEON_API ABaseGameMode : public AChameleonGameMode
 	GENERATED_BODY()
 	
 };
-=======
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
-
-#include "CoreMinimal.h"
-#include "ChameleonGameMode.h"
-#include "BaseGameMode.generated.h"
-
-/**
- * 
- */
-UCLASS()
-class CHAMELEON_API ABaseGameMode : public AChameleonGameMode
-{
-	GENERATED_BODY()
-	
-};
->>>>>>> Stashed changes
